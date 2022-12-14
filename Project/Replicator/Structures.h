c988c9b79f712d68c59586abf760e50548927a0c
Filip Ilic <filipflyer98@gmail.com>
2022-12-14 20:26:02 +0100

    Added replicator to process connection logic without process registration

--- Project/Replicator/Structures.h
+++ Project/Replicator/Structures.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <ws2tcpip.h>
+#include "../Common//HashMap.h"
 
 typedef struct SECONDARY_REPLICATOR_DATA {
 	SOCKET* replicatorSocket;
@@ -14,3 +15,9 @@ typedef struct MAIN_REPLICATOR_DATA {
 	bool* replicatorConnected;
 	HANDLE* FinishSignal;
 }MAIN_REPLICATOR_DATA;
+
+typedef struct REPLICATOR_PROCESS_DATA {
+	SOCKET* listenSocket;
+	HashMap<SOCKET>* processSockets;
+	HANDLE* FinishSignal;
+}REPLICATOR_PROCESS_DATA;
