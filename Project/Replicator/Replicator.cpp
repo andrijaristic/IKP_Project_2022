91893e5d0f070f539b0d6dfa48106cf9d0c7f487
Filip Ilic <filipflyer98@gmail.com>
2022-12-14 14:28:09 +0100

    Added Replicator project

---
+++ Project/Replicator/Replicator.cpp
@@ -0,0 +1,17 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
+#include <string.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+int main()
+{
+    
+}
+
