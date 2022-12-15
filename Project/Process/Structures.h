5134fbf903d9a674d723df904ec124337cf4cb34
andrijaristic <andra.rs@outlook.com>
2022-12-15 18:56:57 +0100

    Added logic for receiving messages from replicator

--- Project/Process/Structures.h
+++ Project/Process/Structures.h
@@ -11,10 +11,15 @@ typedef struct REPLICATOR_DATA{
 	char processId[MAX_PROCESS_ID_LENGTH];
 } REPLICATOR_DATA;
 
-
 typedef struct REPLICATOR_SEND_DATA {
 	HANDLE* FinishSignal;
 	SOCKET* replicatorSocket;
 	char processId[MAX_PROCESS_ID_LENGTH];
 	bool* shutdownSignal;
 } REPLICATOR_SEND_DATA;
+
+typedef struct REPLICATOR_RECEIVE_DATA {
+	HANDLE* FinishSignal;
+	SOCKET* replicatorSocket;
+	char processId[MAX_PROCESS_ID_LENGTH];
+} REPLICATOR_RECEIVE_DATA;
